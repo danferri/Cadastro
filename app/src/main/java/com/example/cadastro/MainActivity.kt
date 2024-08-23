2cc2a7be52dfd95f581f5169b07545a085ff5fd0
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-08-23 15:09:08 -0300

    fix:view biding

--- app/src/main/java/com/example/cadastro/MainActivity.kt
+++ app/src/main/java/com/example/cadastro/MainActivity.kt
@@ -12,63 +12,55 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cadastro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var etNomeCompleto: EditText
-    private lateinit var etTelefone: EditText
-    private lateinit var etEmail: EditText
-    private lateinit var cbEmailList: CheckBox
-    private lateinit var rgSexo: RadioGroup
-    private lateinit var etCidade: EditText
-    private lateinit var spUf: Spinner
-    private lateinit var btnLimpar: Button
-    private lateinit var btnSalvar: Button
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        etNomeCompleto = findViewById(R.id.etNomeCompleto)
-        etTelefone = findViewById(R.id.etTelefone)
-        etEmail = findViewById(R.id.etEmail)
-        cbEmailList = findViewById(R.id.cbEmailList)
-        rgSexo = findViewById(R.id.rgSexo)
-        etCidade = findViewById(R.id.etCidade)
-        spUf = findViewById(R.id.spUf)
-        btnLimpar = findViewById(R.id.btnLimpar)
-        btnSalvar = findViewById(R.id.btnSalvar)
+        setContentView(amb.root)
 
         val ufs = resources.getStringArray(R.array.ufs)
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, ufs)
-        spUf.adapter = adapter
+        amb.spUf.adapter = adapter
 
-        btnSalvar.setOnClickListener {
-            val nomeCompleto = etNomeCompleto.text.toString()
-            val telefone = etTelefone.text.toString()
-            val email = etEmail.text.toString()
-            val ingressarListaEmail = cbEmailList.isChecked
-            val sexo = when (rgSexo.checkedRadioButtonId) {
-                R.id.rbMasculino -> "Masculino"
-                R.id.rbFeminino -> "Feminino"
+        amb.btnSalvar.setOnClickListener {
+            val nomeCompleto = amb.etNomeCompleto.text.toString()
+            val telefone = amb.etTelefone.text.toString()
+            val email = amb.etEmail.text.toString()
+            val ingressarListaEmail = amb.cbEmailList.isChecked
+            val sexo = when (amb.rgSexo.checkedRadioButtonId) {
+                amb.rbMasculino.id -> "Masculino"
+                amb.rbFeminino.id -> "Feminino"
                 else -> "Não especificado"
             }
-            val cidade = etCidade.text.toString()
-            val uf = spUf.selectedItem.toString()
+            val cidade = amb.etCidade.text.toString()
+            val uf = amb.spUf.selectedItem.toString()
 
-            val formulario = Formulario(nomeCompleto, telefone, email, ingressarListaEmail, sexo, cidade, uf)
-            Toast.makeText(this, formulario.toString(), Toast.LENGTH_LONG).show()
+            val formulario = Formulario(
+                nomeCompleto = nomeCompleto,
+                telefone = telefone,
+                email = email,
+                ingressarListaEmail = ingressarListaEmail,
+                sexo = sexo,
+                cidade = cidade,
+                uf = uf
+            )
+            Toast.makeText(this@MainActivity, formulario.toString(), Toast.LENGTH_LONG).show()
         }
 
-
-        btnLimpar.setOnClickListener {
-            etNomeCompleto.text.clear()
-            etTelefone.text.clear()
-            etEmail.text.clear()
-            cbEmailList.isChecked = false
-            rgSexo.clearCheck()
-            etCidade.text.clear()
-            spUf.setSelection(0)
+        amb.btnLimpar.setOnClickListener {
+            amb.etNomeCompleto.text.clear()
+            amb.etTelefone.text.clear()
+            amb.etEmail.text.clear()
+            amb.cbEmailList.isChecked = false
+            amb.rgSexo.clearCheck()
+            amb.etCidade.text.clear()
+            amb.spUf.setSelection(0)
         }
     }
 }
